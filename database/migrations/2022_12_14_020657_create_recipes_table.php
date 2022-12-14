f79b422c0b5320794c6c6181e748bde605a39c9a
Khaled Abd ElRahim <khaled710017@gmail.com>
2022-12-14 04:46:00 +0200

    Create Main migration files

---
+++ database/migrations/2022_12_14_020657_create_recipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Ingredient;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Product::class);
+            $table->foreignIdFor(Ingredient::class);
+            $table->integer('weight')->comment('by grams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
