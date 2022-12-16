030ac33369ea727f884460d9cd4e8afa1fbc78f2
Khaled Abd ElRahim <khaled710017@gmail.com>
2022-12-16 15:52:11 +0200

    creaqte database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Ingredient;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Recipe;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // create ingredients
+        // products
+        // orders
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $beef = Ingredient::factory()->create([
+            'name' => 'Beef',
+            'stock' => 20000,
+        ]);
+        $chicken = Ingredient::factory()->create([
+            'name' => 'Chicken',
+            'stock' => 20000,
+        ]);
+        $cheese = Ingredient::factory()->create([
+            'name' => 'Cheese',
+            'stock' => 5000,
+        ]);
+        $onion = Ingredient::factory()->create([
+            'name' => 'Onion',
+            'stock' => 1000,
+        ]);
+
+
+        $beefBurger = Product::factory()
+            ->create(['name'  => 'Beef Burger', 'price' => 100]);
+        $beefBurger->recipes()->createMany([
+            [
+                'ingredient_id' => $beef->id,
+                'weight' => 150,
+            ], [
+                'ingredient_id' => $cheese->id,
+                'weight' => 30,
+            ], [
+                'ingredient_id' => $onion->id,
+                'weight' => 20,
+            ],
+        ]);
+
+        $chickenBurger = Product::factory()
+            ->create(['name'  => 'Chicken Burger', 'price' => 90]);
+        $chickenBurger->recipes()->createMany([
+            [
+                'ingredient_id' => $chicken->id,
+                'weight' => 150,
+            ], [
+                'ingredient_id' => $cheese->id,
+                'weight' => 30,
+            ], [
+                'ingredient_id' => $onion->id,
+                'weight' => 20,
+            ],
+        ]);
+
+        Order::factory()->create()->products()->attach($chickenBurger);
+
     }
 }
