732f1924f8c94c42e138f16a01089bbd069da90a
Khaled Abd ElRahim <khaled710017@gmail.com>
2022-12-17 22:07:46 +0200

    Add mail event/content

---
+++ app/Mail/OutOfStockNotifyMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Ingredient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OutOfStockNotifyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  Ingredient  $ingredient
+     * @return void
+     */
+    public function __construct(public Ingredient $ingredient)
+    {
+        //
+    }
+
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('inventory@fooodics.com', 'Foodics Inventory'),
+            subject: 'Out Of Stock Notify Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.stock',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/stock.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <p> Dear refillment Guy,</p>
+    <p> Hope you're doing well, </p>
+
+    <p> We need an urgent refill for <b>{{ $ingredient->name }}</b>. </p>
+
+    <p> Best oof luck.. </p>
+</div>
