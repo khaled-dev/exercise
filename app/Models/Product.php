19aefceeb1e10ddfb143b7c34f6edd7cbd7c92a5
Khaled Abd ElRahim <khaled710017@gmail.com>
2022-12-17 16:38:54 +0200

    Add IngredientQuantity group selection query

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
@@ -24,6 +25,15 @@ class Product extends Model
     public function recipes(): HasMany
     {
         return $this->hasMany(Recipe::class);
+    }
+
+    public function scopeIngredientQuantity(Builder $q, array $productIds)
+    {
+        return $q->selectRaw('recipes.ingredient_id as id, ingredients.name as name, sum(recipes.weight) as weights')
+            ->leftJoin('recipes', 'recipes.product_id', '=','products.id')
+            ->leftJoin('ingredients', 'ingredients.id', '=','recipes.ingredient_id')
+            ->whereIn('products.id', $productIds)
+            ->groupBy('recipes.ingredient_id');
     }
 
     public function ingredients(): BelongsToMany
